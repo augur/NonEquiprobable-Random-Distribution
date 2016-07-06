a809175f8048c67a6a3fe5b66447a41916834d5a
Denis Kilchichakov <denis@kilchichakov.com>
2016-07-07 00:27:31 +0300

    Implemented trivial_solver, added test stub

--- trivial_solver.cpp
+++ trivial_solver.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  /* stub */
-  return 0;
+  double rvalue;
+  cin >> rvalue;
+  int dist_count;
+  cin >> dist_count;
+
+  double* distribution = new double[dist_count];
+  for (size_t i = 0; i < dist_count; i++) {
+    cin >> distribution[i];
+  }
+
+  //This solution can fit in first loop, but trivial_solver should be
+  //straightforward (if not dumb)
+  double counter = 0;
+  for (size_t i = 0; i < dist_count; i++) {
+    counter += distribution[i];
+    if (rvalue < counter) {
+      cout << i;
+      return 0;
+    }
+  }
+
+  cout << "error";
+  return 1;
 }
